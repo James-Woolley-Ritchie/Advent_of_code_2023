211e9c70afcabdc4bec8f32a39b59b1e850a0d31
James Woolley-Ritchie <james.woolley-ritchie@cgi.com>
2023-12-06 11:30:12 +0000

    Day 5 part 1 & 2

--- Day_5/part_1.py
+++ Day_5/part_1.py
@@ -1,25 +1,82 @@
-with open("Day_5\\input.txt", "r") as file:
-    data = file.read()
-    sources, *data_blocks = data.split("\n\n")
+lowest_location_number = None
 
-sources = list(map(int, sources.split(":")[1].split()))
+seeds = []
+seed_to_soil = []
+soil_to_fertilizer = []
+fertilizer_to_water = []
+water_to_light = []
+light_to_temperature = []
+temperature_to_humidity = []
+humidity_to_location = []
 
-for data_block in data_blocks:
-    ranges_list = []
-    for line in data_block.splitlines()[1:]:
-        ranges_list.append(list(map(int, line.split())))
+def Find_in_map(map, key):
+    for i in range(len(map)):
+        if int(key) in range(int(map[i][1]), int(map[i][1]) + int(map[i][2]) - 1): # If it's in the defined range.
+            return int(map[i][0]) + int(key) - int(map[i][1])
+        
+    return None
 
-    destinations = []
+# Iterate through the input.
+with open("Day_5\\input.txt", "r") as input:
+    # Get the lists of seeds.
+    seeds = input.readline().replace("\n", "").split(":")[1].strip().split(" ")
+    input.readline() # Skip the empty line.
 
-    for current_source in sources:
-        for destination, source, length in ranges_list:
-            if current_source in range(source, source + length + 1):
-                destinations.append(current_source - source + destination)
-                break
+    # Go through the lines containing the maps.
+    current_map = ""
+    for line in input:
+        if "map" in line.lower():
+            current_map = line.split(" ")[0]
+        
         else:
-            destinations.append(current_source)
+            if line.strip() != "":
+                if current_map == "seed-to-soil":
+                    seed_to_soil.append(line.strip().split(" "))
+                elif current_map == "soil-to-fertilizer":
+                    soil_to_fertilizer.append(line.strip().split(" "))
+                elif current_map == "fertilizer-to-water":
+                    fertilizer_to_water.append(line.strip().split(" "))
+                elif current_map == "water-to-light":
+                    water_to_light.append(line.strip().split(" "))
+                elif current_map == "light-to-temperature":
+                    light_to_temperature.append(line.strip().split(" "))
+                elif current_map == "temperature-to-humidity":
+                    temperature_to_humidity.append(line.strip().split(" "))
+                elif current_map == "humidity-to-location":
+                    humidity_to_location.append(line.strip().split(" "))
 
-    sources = destinations
+for seed in seeds:
+    soil = Find_in_map(seed_to_soil, seed)
+    if soil == None: 
+        soil = seed
+    
+    fertilizer = Find_in_map(soil_to_fertilizer, soil)
+    if fertilizer == None:
+        fertilizer = soil
 
-smallest_location = min(destinations)
-print(smallest_location)
+    water = Find_in_map(fertilizer_to_water, fertilizer)
+    if water == None:
+        water = fertilizer
+
+    light = Find_in_map(water_to_light, water)
+    if light == None:
+        light = water
+
+    temperature = Find_in_map(light_to_temperature, light)
+    if temperature == None:
+        temperature = light
+
+    humidity = Find_in_map(temperature_to_humidity, temperature)
+    if humidity == None:
+        humidity = temperature
+
+    location = Find_in_map(humidity_to_location, humidity)
+    if location == None:
+        location = humidity
+
+    if lowest_location_number == None: # Not been set yet.
+        lowest_location_number = location
+    elif location < lowest_location_number:
+        lowest_location_number = location
+
+print(lowest_location_number)
--- Day_5/part_2.py
+++ Day_5/part_2.py
@@ -1,1 +1,96 @@
-# It's 03:03, f*ck this.
+lowest_location_number = None
+
+seeds = []
+seed_to_soil = []
+soil_to_fertilizer = []
+fertilizer_to_water = []
+water_to_light = []
+light_to_temperature = []
+temperature_to_humidity = []
+humidity_to_location = []
+
+def Find_in_map(map, key):
+    for i in range(len(map)):
+        if int(key) in range(int(map[i][1]), int(map[i][1]) + int(map[i][2]) + 1): # If it's in the defined range.
+            return int(map[i][0]) + int(key) - int(map[i][1])
+        
+    return None
+
+def Find_from_value_in_map(map, value):
+    for i in range(len(map)):
+        if int(value) in range(int(map[i][0]), int(map[i][0]) + int(map[i][2]) - 1):
+            return int(map[i][1]) + int(value) - int(map[i][0])
+        
+    return None
+
+# Iterate through the input.
+with open("Day_5\\input.txt", "r") as input:
+    # Get the lists of seeds.
+    top_line = input.readline().replace("\n", "").split(":")[1].strip().split(" ")
+    i = 0
+    while i in range(len(top_line)):
+        seeds.append([top_line[i], top_line[i+1]])
+        i += 2
+    input.readline() # Skip the empty line.
+
+    # Go through the lines containing the maps.
+    current_map = ""
+    for line in input:
+        if "map" in line.lower():
+            current_map = line.split(" ")[0]
+        
+        else:
+            if line.strip() != "":
+                if current_map == "seed-to-soil":
+                    seed_to_soil.append(line.strip().split(" "))
+                elif current_map == "soil-to-fertilizer":
+                    soil_to_fertilizer.append(line.strip().split(" "))
+                elif current_map == "fertilizer-to-water":
+                    fertilizer_to_water.append(line.strip().split(" "))
+                elif current_map == "water-to-light":
+                    water_to_light.append(line.strip().split(" "))
+                elif current_map == "light-to-temperature":
+                    light_to_temperature.append(line.strip().split(" "))
+                elif current_map == "temperature-to-humidity":
+                    temperature_to_humidity.append(line.strip().split(" "))
+                elif current_map == "humidity-to-location":
+                    humidity_to_location.append(line.strip().split(" "))
+found = False
+lowest_location_number = 0
+
+while not found:
+    humidity = Find_from_value_in_map(humidity_to_location, lowest_location_number)
+    if humidity == None:
+        humidity = lowest_location_number
+    
+    temperature = Find_from_value_in_map(temperature_to_humidity, humidity)
+    if temperature == None:
+        temperature = humidity
+
+    light = Find_from_value_in_map(light_to_temperature, temperature)
+    if light == None:
+        light = temperature
+
+    water = Find_from_value_in_map(water_to_light, light)
+    if water == None:
+        water = light
+
+    fertilizer = Find_from_value_in_map(fertilizer_to_water, water)
+    if fertilizer == None:
+        fertilizer = water
+
+    soil = Find_from_value_in_map(soil_to_fertilizer, fertilizer)
+    if soil == None:
+        soil = fertilizer
+
+    seed = Find_from_value_in_map(seed_to_soil, soil)
+    if seed == None:
+        seed = soil
+
+    for possible_seed in seeds:
+        if int(seed) in range(int(possible_seed[0]), int(possible_seed[0]) + int(possible_seed[1]) - 1):
+            found = True
+            break
+
+print(lowest_location_number)
+
